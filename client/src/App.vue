b179a0b40699d6d0368820f93d94576203e0a2d0
avischool <avi_kumar@horacemann.org>
2022-02-23 12:53:33 -0500

    add computed stuff

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <div id="griddiv">
-    <Grid :rowLetters="sample"/>
+    <Grid :rowLetters="wordList"/>
 
   </div>
   
@@ -12,14 +12,30 @@
 import HelloWorld from './components/HelloWorld.vue'
 import Grid from '@/components/grid/Grid.vue'
 
+/*
+Component
+data -> raw variable/information
+Computed -> processed information
+Array.from -> array builder
+Truthy variable
+Ternary operater -> boolean ? true_value : false_value
+Map Function -> (_, idx) => ternary operator
+
+*/
+
 export default {
   name: 'App',
   data() {
     return {
       message: "Welcome to our Wordle!",
       letters:"HELLO",
-      sample:["hello","hello"],
+      words:["hello","daily"],
     }
+  },
+  computed: {
+    wordList() {
+      return Array.from({ length:6 }, (_,idx) => this.words[idx] ? this.words[idx] : "     ");
+    },
   },
   components: {
     HelloWorld,
